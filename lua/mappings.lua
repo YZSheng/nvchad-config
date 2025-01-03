46e8e00501b2f832e40becbf26639990eb0a472a
Sheng Yunzhou <shengyunzhou@gmail.com>
2025-01-03 12:09:45 +0800

    Update config

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,13 +8,20 @@ map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
-local wk = require("which-key")
+local wk = require "which-key"
 
 wk.add({
   { "<leader>l", group = "LSP" }, -- Create an "LSP" group
-  { "<leader>ln", function() vim.lsp.buf.rename() end, desc = "Rename symbol" }, -- Rename function
+  {
+    "<leader>ln",
+    function()
+      vim.lsp.buf.rename()
+    end,
+    desc = "Rename symbol",
+  }, -- Rename function
 }, { mode = "n" })
 
 map("v", "J", ":m '>+1<CR>gv=gv")
 map("v", "K", ":m '<-2<CR>gv=gv")
-
+map("n", "<D-H>", ":bprevious<CR>")
+map("n", "<D-L>", ":bnext<CR>")
